b5b61a6c1e8400fa5ca33c2076cb23d2161fe40d
lorenzo-mignola <lo.mignola@gmail.com>
2022-07-24 19:08:25 +0200

    Add material components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,61 +1,35 @@
-<script context="module" lang="ts">
-	export const prerender = true;
-</script>
-
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	import * as d3 from 'd3';
+	import { onMount } from 'svelte';
+
+	var data = [30, 86, 168, 281, 303, 365];
+
+	let element: HTMLElement;
+
+	onMount(() => {
+		d3.select(element)
+			.selectAll('div')
+			.data(data)
+			.enter()
+			.append('div')
+			.style('width', function (d) {
+				return d + 'px';
+			})
+			.text(function (d) {
+				return d;
+			});
+	});
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<div bind:this={element} class="chart" />
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.chart :global(div) {
+		font: 10px sans-serif;
+		background-color: steelblue;
+		text-align: right;
+		padding: 3px;
+		margin: 1px;
+		color: white;
 	}
 </style>
